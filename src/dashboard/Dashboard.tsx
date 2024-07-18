70fc161f872ce12761c9ee36b17ee5d4d9254dcb
doyarin-dev <syokunin0614@gmail.com>
2024-07-18 19:34:28 +0900

    カードを追加するロジック追加

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -13,13 +13,17 @@ import Badge from '@mui/material/Badge';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
+import DeleteIcon from '@mui/icons-material/Delete';
 import MenuIcon from '@mui/icons-material/Menu';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import { AddCard } from '@mui/icons-material';
 
 const drawerWidth: number = 240;
 
@@ -76,9 +80,20 @@ const defaultTheme = createTheme();
 
 export default function Dashboard() {
   const [open, setOpen] = React.useState(true);
+  const [components, setComponents] = React.useState<JSX.Element[]>([]); // 追加するコンポーネントを保持する配列
   const toggleDrawer = () => {
     setOpen(!open);
   };
+
+  const addCard = () => {
+    const newComponent = <Grid item xs={3}><TextField
+      id="outlined-multiline-static"
+      multiline
+      rows={4}
+      defaultValue=""
+    /></Grid>;
+    setComponents([...components, newComponent]);
+  }
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -152,38 +167,11 @@ export default function Dashboard() {
         >
           <Toolbar />
           <Container maxWidth={false} sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              {/* Chart */}
-              <Grid item xs={12} md={8} lg={9}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                </Paper>
-              </Grid>
-              {/* Recent Deposits */}
-              {/* <Grid item xs={12} md={4} lg={3}>
-                <Paper
-                  sx={{
-                    p: 2,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: 240,
-                  }}
-                >
-                  <Deposits />
-                </Paper>
-              </Grid> */}
-              {/* Recent Orders */}
-              {/* <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                  <Orders />
-                </Paper>
-              </Grid> */}
+            <IconButton aria-label="delete" size="large" onClick={addCard}>
+              <AddCircleIcon fontSize="inherit" />
+            </IconButton>
+            <Grid container spacing={2}>
+              {components}
             </Grid>
           </Container>
         </Box>
